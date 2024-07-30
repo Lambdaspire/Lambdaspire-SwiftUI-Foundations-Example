6d55827d096ab752fea72de2e7371713e26eb432
Skylark <skylark@lambdaspire.com>
2024-07-30 13:44:03 +1000

    dumb wip

--- LambdaspireSwiftUIFoundationsExample/Services/Api/RealApiService.swift
+++ LambdaspireSwiftUIFoundationsExample/Services/Api/RealApiService.swift
@@ -7,6 +7,6 @@ import LambdaspireDependencyResolution
 class RealApiService : ApiService {
     func getData() async throws -> String {
         try await Task.sleep(nanoseconds: UInt64.random(in: 1000000...10000000))
-        return Date().ISO8601Format()
+        return UUID().uuidString
     }
 }
